5a0f4e37380bde77a1a5dac7b3384388b3355fb6
mallepalli <sukeshmallepalli@gmail.com>
2016-02-22 09:21:40 +0100

    auto build staging

---
+++ staging.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd /C/Users/Sukesh/Documents/GitHub/Trustpilot
+git fetch --prune
+git checkout origin/master
+git branch -D staging
+git checkout -b staging
+echo -------------------------------
+for service in C:\buildStaging.py
+do
+echo Merging $service
+git merge --no-ff origin/$service | grep 'failed' &> /dev/null 
+if [ $? == 0 ]; then
+   git merge --abort
+   echo "MERGE IS ABORTED FOR THE CARD" $service  
+else
+	echo Merge is done
+fi
+
+echo ------------------------------------
+sleep 1
+done
+git push origin staging -f
